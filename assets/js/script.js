d3d3b0ecc4cd300b14fdceddabf50efdea21232a
SandersonHub <128574459+SandersonHub@users.noreply.github.com>
2023-06-25 14:12:35 -0400

    updated javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,28 @@
 document.getElementById("search-form").addEventListener("submit", async function (event) {
   event.preventDefault();
 
-  //input for the city name, may add something to this in the future to be clear to the user.
-  let city = document.getElementById("city-input").value.trim();
-  if (city === "") return;
+  // Input for the city name, may add something to this in the future to be clear to the user.
+  let cityOfChoice = document.getElementById("city-input").value.trim();
+  if (cityOfChoice === "") return;
 
-  let apiKey = "e932e508bcb29838927bef8e0efbe316";
-  let [location] = await (await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apiKey}`)).json();
+  const apiKey = "e932e508bcb29838927bef8e0efbe316";
+  let [location] = await (await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${cityOfChoice}&limit=1&appid=${apiKey}`)).json();
   if (!location) {
     console.log("No results found.");
     return;
   }
-//calls for the lat lon for the location of the weather
+
+  // Calls for the latitude and longitude for the location of the weather
   let { lat, lon } = location;
-//fetch weather
-  let weatherData = await (await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`)).json();
-  let temperatureFahrenheit = ((weatherData.main.temp - 273.15) * 9 / 5) + 32;
-  document.getElementById("current-weather-info").textContent = `Temperature: ${temperatureFahrenheit.toFixed(2)}°F`;
+
+  // Fetch weather information
+  let weatherInfo = await (await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`)).json();
+  let temperatureFahrenheit = ((weatherInfo.main.temp - 273.15) * 9 / 5) + 32;
+  document.getElementById("current-weather-information").textContent = `Temperature: ${temperatureFahrenheit.toFixed(2)}°F`;
 
   let forecastData = await (await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`)).json();
 
-  let forecastInfo = document.getElementById("forecast-info");
+  let forecastInfo = document.getElementById("forecast-information");
   forecastInfo.innerHTML = "";
 
   forecastData.list
@@ -28,7 +30,7 @@ document.getElementById("search-form").addEventListener("submit", async function
     .forEach(forecastItem => {
       let forecastDate = new Date(forecastItem.dt_txt);
       let temperatureFahrenheit = ((forecastItem.main.temp - 273.15) * 9 / 5) + 32;
-
+//inline HMTL, to change the temperture
       let forecastElement = document.createElement("div");
       forecastElement.innerHTML = `
         <p>Date: ${forecastDate.toLocaleDateString()}</p>
@@ -37,7 +39,8 @@ document.getElementById("search-form").addEventListener("submit", async function
 
       forecastInfo.appendChild(forecastElement);
     });
-//history list of the cities search for
+
+  // History list of the cities searched for
   let historyList = document.getElementById("history-list");
   let listItem = document.createElement("li");
   listItem.textContent = location.name;
